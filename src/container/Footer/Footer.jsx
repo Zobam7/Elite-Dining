ad95a0690af1e23953afc640ecc58c22e59a0607
Zobam7 <zobamokoli7@gmail.com>
2022-03-13 12:50:46 +0100

    third commit

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => (
         <h1 className="app__footer-headtext">Contact Us</h1>
         <p className="p__opensans">Lagos, Nigeria</p>
         <p className="p__opensans">+2349023589304</p>
-        <p className="p__opensans">+2349023589304</p>
+        <p className="p__opensans email"><a href="mailto:zobamokoli7@gmail.com" target="_blank">Zobamokoli7@gmail.com</a></p>
       </div>
 
       <div className="app__footer-links_logo">
@@ -23,7 +23,6 @@ const Footer = () => (
         <p className="p__opensans">"The best way to find yourself is to lose yourself in the service of others"</p>
         <img src={images.spoon} className="spoon__img" style={{marginTop: 15}} alt="" />
         <div className="app__footer-links_icons">
-          <FiFacebook />
           <FiInstagram />
           <FiTwitter />  
         </div>
